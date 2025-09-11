08582b5457f39510c4bd6cdf8610bd77bcc8abc6
Anagha Deogaonkar <anagha.deogaonkar01@gmail.com>
2025-09-11 10:58:43 -0500

    Time: 361 ms (39.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,9 @@
+WITH report_count AS(
+    SELECT managerId
+    FROM Employee
+    Group by managerId
+    HAVING count(id) >= 5
+)
+SELECT name
+FROM Employee
+WHERE id IN (SELECT managerId FROM report_count)
