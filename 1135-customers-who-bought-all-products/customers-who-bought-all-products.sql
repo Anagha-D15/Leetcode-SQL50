a375b15c601385a76c22f51d975688baa331a8cf
Anagha Deogaonkar <anagha.deogaonkar01@gmail.com>
2025-10-23 19:17:06 -0500

    Time: 522 ms (73.19%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -2,4 +2,4 @@
  SELECT customer_id 
  FROM Customer 
  GROUP BY customer_id
- HAVING count(DISTINCT product_key)=(SELECT  count(product_key)FROM Product) 
+ HAVING count(DISTINCT product_key)=(SELECT count(product_key)FROM Product) 
