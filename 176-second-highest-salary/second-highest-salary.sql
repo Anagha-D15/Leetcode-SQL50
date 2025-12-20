f1d743aa0d179cf8ff0570b064b00211e88f6716
Anagha Deogaonkar <anagha.deogaonkar01@gmail.com>
2025-12-20 10:02:41 -0600

    Time: 272 ms (36.54%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT (
+    SELECT salary
+    FROM (
+        SELECT DISTINCT
+               salary,
+               DENSE_RANK() OVER (ORDER BY salary DESC) AS ranking
+        FROM Employee
+    ) t
+    WHERE ranking = 2
+) AS SecondHighestSalary;
+
+
+
