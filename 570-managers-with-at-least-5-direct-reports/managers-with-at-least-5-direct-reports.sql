87bda59f8234a2b5a77e7e22ede2892f43d2107a
Anagha Deogaonkar <anagha.deogaonkar01@gmail.com>
2026-01-21 13:33:19 -0600

    Time: 353 ms (31.43%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,14 @@
-SELECT e1.name
-FROM Employee e1 INNER JOIN Employee e2
-ON e1.id=e2.managerId
-GROUP BY e1.id
-HAVING count(e2.managerID) > 4
+SELECT name
+FROM Employee
+WHERE id 
+IN
+(SELECT managerId 
+FROM Employee
+GROUP BY managerId
+HAVING count(id) >4 )
+
+
+
 
 
 
